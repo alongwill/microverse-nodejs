497e8a69e06d13bba9215f76e6c50b1f3636e6ea
alongwill <andrewlongwill@gmail.com>
2017-03-13 19:41:29 +0000

    Testing ES6 features

--- milestone2/simple/example.js
+++ milestone2/simple/example.js
@@ -6,14 +6,32 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
+function Prefixer(prefix) {
+      this.prefix = prefix;
+}
+
+// Prefixer.prototype.prefixArray = function (arr) {
+//       return arr.map(function (x) {
+//       return this.prefix + x;
+//       }, this); // (A)
+// };
+Prefixer.prototype.prefixArray = function (arr) {
+      return arr.map((x) => this.prefix + x); // (A)
+};
+
+const arr = ['a','b','c'];
+p = new Prefixer("PRE");
+
+
 const server = http.createServer((req, res) => {
       res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-          res.end('Hello World\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Hello World\n');
 
 });
 
 server.listen(port, hostname, () => {
       console.log(`Server running at http://${hostname}:${port}/`);
-
+      console.log(p.prefixArray(arr));
 });
