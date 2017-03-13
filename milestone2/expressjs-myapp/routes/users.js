4317bcb044e2c6ea9860c2e6b41eae9b65ec0792
alongwill <andrewlongwill@gmail.com>
2017-03-13 18:31:35 +0000

    ExpressJS routing tests

--- milestone2/expressjs-myapp/routes/users.js
+++ milestone2/expressjs-myapp/routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.put('/', function (req, res) {
+  res.send('Got a PUT request at /users')
+})
+router.delete('/', function (req, res) {
+  res.send('Got a DELETE request at /users')
+})
+
 module.exports = router;
