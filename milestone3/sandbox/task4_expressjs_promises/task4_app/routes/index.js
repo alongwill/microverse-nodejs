7b186d41679f542a290dd2d8b15b6d604f9dac14
alongwill <andrewlongwill@gmail.com>
2017-04-10 22:30:37 +0100

    Testing ExpressJS error handling

--- milestone3/sandbox/task4_expressjs_promises/task4_app/routes/index.js
+++ milestone3/sandbox/task4_expressjs_promises/task4_app/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  throw new Error('oh no!','foo');
+  //res.render('index', { title: 'Express' });
 });
-
+router.use(function (err, req, res, next) {
+  console.log(err.message) // oh no!
+  res.render('error', { title: 'Express' });
+})
 module.exports = router;
