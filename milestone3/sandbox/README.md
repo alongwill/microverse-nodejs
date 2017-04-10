aa86265bb00c9cdb2c3dda162146904b153c2a1f
alongwill <andrewlongwill@gmail.com>
2017-04-10 21:36:14 +0100

    Updated Promise example

---
+++ milestone3/sandbox/README.md
@@ -0,0 +1,10 @@
+# Milestone 3 Sandbox
+
+## Tasks
+
+### #3 ES6 Promises
+
+[task3_promises.js](task3_promises.js)
+* [Promise](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Promise) (developer.mozilla.org)
+
+
--- milestone3/sandbox/task3_promises.js
+++ milestone3/sandbox/task3_promises.js
@@ -1,6 +1,7 @@
 /*
  * https://nodejs.org/api/synopsis.html
  */
+
 var fs = require('fs');
 
 /*
@@ -14,6 +15,7 @@ fs.readFile('config.json',
             try {
                 const obj = JSON.parse(text);
                 console.log('---------------------------');
+                console.log('*** Using standard fucntion ***');
                 console.log(JSON.stringify(obj, null, 4));
                 console.log('---------------------------');
             } catch (e) {
@@ -30,23 +32,24 @@ var promise = new Promise(function(resolve, reject) {
   fs.readFile('config.json',
 	function (error, text) {
         if (error) {
-            //console.error('Error while reading config file: ' + error.message);
-            reject(Error('Error while reading config file: ' + error.message));
+            reject(error.message);
         } else {
             try {
                 const obj = JSON.parse(text);
                 resolve(JSON.stringify(obj, null, 4));
-
             } catch (e) {
-                reject(Error('Invalid JSON in file'));
+                reject("Error parsing JSON: " + e);
             }
         }
     });
 });
 
-promise()
+promise
 .then(function(result) {
+    console.log('---------------------------');
+    console.log('*** Using a promise ***');
     console.log(result);
+    console.log('---------------------------');
 })
 .catch(function (error) { // (B)
     // File read error or JSON SyntaxError
